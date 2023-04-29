5e733ab7974fe0fb3ee4af3e4bcd2c68948abbf2
xRayMonDxp <raymond980711@gmail.com>
2023-04-29 17:44:25 -0400

    make transporter async

--- api/gmail.js
+++ api/gmail.js
@@ -15,6 +15,19 @@ let transporter = nodemailer.createTransport({
 	},
 });
 
+await new Promise((resolve, reject) => {
+	// verify connection configuration
+	transporter.verify(function (error, success) {
+		if (error) {
+			console.log(error);
+			reject(error);
+		} else {
+			console.log('Server is ready to take our messages');
+			resolve(success);
+		}
+	});
+});
+
 async function sendMail(mailPayload) {
 	let mailOptions = {
 		from: mailPayload.name + ' <noreply.raymondleemv@gmail.com>',
