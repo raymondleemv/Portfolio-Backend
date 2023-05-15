2130608c4b0077c06d0179c7c2b1c803bcb22984
xRayMonDxp <raymond980711@gmail.com>
2023-05-15 16:08:52 -0400

    added maximum file size settings in app to accept large base64 string in requests

--- api/index.js
+++ api/index.js
@@ -13,8 +13,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(
 	cors({
